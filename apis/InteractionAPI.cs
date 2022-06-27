f9f9fb4c8d9782bd4a43100334ae8d7831064d22
Cyber28 <cybergaming0628@gmail.com>
2022-06-27 21:32:51 +0200

    added support for CardType interactions

--- apis/InteractionAPI.cs
+++ apis/InteractionAPI.cs
@@ -8,14 +8,18 @@ namespace BerryLoaderNS
 	{
 		public delegate void CanHaveCardDataHandler(CardData instance, CardData otherCard, ref bool? result);
 
-		public static Dictionary<string, List<CanHaveCardDataHandler>> Interactions = new Dictionary<string, List<CanHaveCardDataHandler>>();
+		public static Dictionary<string, List<CanHaveCardDataHandler>> IdInteractions = new Dictionary<string, List<CanHaveCardDataHandler>>();
+		public static Dictionary<CardType, List<CanHaveCardDataHandler>> CardTypeInteractions = new Dictionary<CardType, List<CanHaveCardDataHandler>>();
 		public static List<Type> CardDatas = new List<Type>();
 
-		public static void CreateInteraction(string id, CanHaveCardDataHandler f)
+		public static void CreateIdInteraction(string id, CanHaveCardDataHandler f)
 		{
-			if (!Interactions.ContainsKey(id))
-				Interactions[id] = new List<CanHaveCardDataHandler>();
-			Interactions[id].Add(f);
+			AddInteraction<string>(IdInteractions, id, f);
+		}
+
+		public static void CreateCardTypeInteraction(CardType t, CanHaveCardDataHandler f)
+		{
+			AddInteraction<CardType>(CardTypeInteractions, t, f);
 		}
 
 		public static void Init()
@@ -33,21 +37,30 @@ namespace BerryLoaderNS
 
 		public static bool CanHaveCardPatch(CardData __instance, CardData otherCard, ref bool __result)
 		{
-			if (Interactions.ContainsKey(__instance.Id))
+			var funcs = new List<CanHaveCardDataHandler>();
+			if (IdInteractions.ContainsKey(__instance.Id))
+				funcs.AddRange(IdInteractions[__instance.Id]);
+			if (CardTypeInteractions.ContainsKey(__instance.MyCardType))
+				funcs.AddRange(CardTypeInteractions[__instance.MyCardType]);
+
+			foreach (var f in funcs)
 			{
-				BerryLoader.L.LogInfo(Interactions[__instance.Id].Count);
-				foreach (var f in Interactions[__instance.Id])
+				bool? res = null;
+				f(__instance, otherCard, ref res);
+				if (res != null)
 				{
-					bool? res = null;
-					f(__instance, otherCard, ref res);
-					if (res != null)
-					{
-						__result = (bool)res;
-						return false;
-					}
+					__result = (bool)res;
+					return false;
 				}
 			}
 			return true;
+		}
+
+		private static void AddInteraction<T>(Dictionary<T, List<CanHaveCardDataHandler>> dict, T key, CanHaveCardDataHandler f)
+		{
+			if (!dict.ContainsKey(key))
+				dict[key] = new List<CanHaveCardDataHandler>();
+			dict[key].Add(f);
 		}
 	}
 }
