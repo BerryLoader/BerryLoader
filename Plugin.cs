db88187079442cd6e52b3284aa96f46f9aa71df4
Cyber28 <cybergaming0628@gmail.com>
2023-01-02 14:43:25 +0100

    version bump

--- Plugin.cs
+++ Plugin.cs
@@ -16,7 +16,7 @@ namespace BerryLoaderNS
 	[BepInProcess("Stacklands.exe")]
 	public class BerryLoader : BaseUnityPlugin
 	{
-		public const string VERSION = "0.6.2";
+		public const string VERSION = "0.7.0";
 		public static BepInEx.Logging.ManualLogSource L;
 		public static BerryLoader instance;
 
